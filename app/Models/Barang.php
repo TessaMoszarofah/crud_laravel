2c54ae6099fbec879870549c2ce66b3cd9808132
TessaMoszarofah <tessamoszarofah@gmail.com>
2024-05-25 22:24:13 +0700

    first commit

---
+++ app/Models/Barang.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Barang extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nama_barang', 'harga', 'stok','id_merek'];
+    protected $visible = ['nama_barang', 'harga', 'stok','id_merek'];
+
+    public function merek(){
+        return $this->hasMany(Merek::class);
+    }
+}
---
+++ app/Models/Merek.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Merek extends Model
+{
+    use HasFactory;
+    protected $fillable = ['nama_merek'];
+    protected $visible = ['nama_merek'];
+    
+    public function barang(){
+        return $this->BelongsTo(Barangs::class,'id_barang');
+    }
+}
